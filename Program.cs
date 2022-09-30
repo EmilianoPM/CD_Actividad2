aa06a9448f5d4eb56b6f293afb4e8f09ed953f80
EmilianoPM <114167867+EmilianoPM@users.noreply.github.com>
2022-09-29 21:42:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_Act2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("CD_Actividad2");
+        }
+
+    }
+}
